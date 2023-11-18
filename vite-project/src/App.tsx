ef857a05eebbd7d9d8de7e261d0bb04d6f1a1a60
TLRKiliann <philogenie@protonmail.com>
2023-11-18 13:26:36 +0100

    first animation added

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,6 +1,35 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+
+  //const [count, setCount] = useState<number>(0);
+  const [isActivate, setIsActivate] = useState<boolean>(false);
+
+  const handleClickBtn1 = () => {
+    console.log("clicked 1")
+  }
+
+  const handleClickBtn2 = () => {
+    console.log("clicked 2")
+  }
+
+  const handleClickBtn3 = () => {
+    setIsActivate(true);
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (isActivate === true) {
+        setIsActivate(false)
+      }
+    }, 1000)
+    timer;
+    return () => {
+      console.log("useEffect done!")
+      clearTimeout(timer);
+    }
+  }, [isActivate])
 
   return (
     <main>
@@ -15,22 +44,28 @@ function App() {
           <p className='square_2--paragraph2'>text 2</p>
         </div>
 
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
+        <div id='square1' className={isActivate === false ? 'square' : 'activated-square'}></div>
+        <div id='square2' className={isActivate === false ? 'square' : 'activated-square'}></div>
+        <div id='square3' className={isActivate === false ? 'square' : 'activated-square'}></div>
 
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
+        <div id='square4' className={isActivate === false ? 'square' : 'activated-square'}></div>
+        <div id='square5' className={isActivate === false ? 'square' : 'activated-square'}></div>
+        <div id='square6' className={isActivate === false ? 'square' : 'activated-square'}></div>
 
-        <div className='square'></div>
-        <div className='square'></div>
-        <div className='square'></div>
+        <div id='square7' className={isActivate === false ? 'square' : 'activated-square'}></div>
+        <div id='square8' className={isActivate === false ? 'square' : 'activated-square'}></div>
+        <div id='square9' className={isActivate === false ? 'square' : 'activated-square'}></div>
 
-        <button className='button'>Snake</button>
-        <button className='button'>K-2000</button>
-        <button className='button'>one-after-one</button>
+        <button type="button" onClick={handleClickBtn1} className='button'>Snake</button>
+        <button type="button" onClick={handleClickBtn2} className='button'>K-2000</button>
+        <button type="button" onClick={handleClickBtn3} className='button'>one-after-one</button>
 
+      </div>
+
+      <div >
+        <p>
+          Counter: {/*count*/}
+        </p>
       </div>
 
     </main>
