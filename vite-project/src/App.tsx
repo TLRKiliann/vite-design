4adc8e5a84589cde146640166aaf1c48ffc57a71
TLRKiliann <philogenie@protonmail.com>
2023-11-19 01:51:19 +0100

    third animation & react-icons added

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react';
+import { FaReact, FaGithub, FaStackOverflow, FaSass } from "react-icons/fa";
+import { SiTypescript, SiTailwindcss, SiMysql } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+
 import './App.css';
 
 type ActivateProps = {
   boolOne: boolean;
   boolTwo: boolean;
+  boolThree: boolean;
 }
 
 function App() {
@@ -11,11 +16,16 @@ function App() {
   const [count, setCount] = useState<number>(0);
   const [isActivate, setIsActivate] = useState<ActivateProps>({
     boolOne: false,
-    boolTwo: false
+    boolTwo: false,
+    boolThree: false
   });
 
   const handleClickBtn1 = () => {
-    console.log("clicked 1");
+    setIsActivate({
+      ...isActivate,
+        boolOne: true
+    })
+    setCount(prev => prev + 1);
   }
 
   const handleClickBtn2 = () => {
@@ -27,10 +37,7 @@ function App() {
   }
 
   const handleClickBtn3 = () => {
-    setIsActivate({
-      ...isActivate,
-        boolOne: true
-    })
+    setIsActivate((prev) => ({...isActivate, boolThree: !prev.boolThree}))
     setCount(prev => prev + 1);
   }
 
@@ -42,7 +49,7 @@ function App() {
             boolOne: false
         })
       }
-    }, 2000)
+    }, 2700)
     timer;
     return () => {
       clearTimeout(timer);
@@ -58,18 +65,21 @@ function App() {
             boolTwo: false
         })
       }
-    }, 2000)
+    }, 3200)
     timer2;
     return () => {
       clearTimeout(timer2);
       console.log("useEffect done! (2)");
     }
   }, [isActivate.boolTwo]);
-  
+
   console.log(isActivate.boolOne, "isActivate.boolOne");
   console.log(isActivate.boolTwo, "isActivate.boolTwo");
-
   console.log(count, "count");
+
+  let boolOne: boolean = isActivate.boolOne;
+  let boolTwo: boolean = isActivate.boolTwo;
+  let boolThree: boolean = isActivate.boolThree;
 
   return (
     <main>
@@ -77,64 +87,78 @@ function App() {
       <div className='grid'>
 
         <div className='square-top square_1'>
-          <p className='square_1--paragraph1'>text 1</p>
+          <p className='square_1--paragraph1'>Have</p>
         </div>
 
         <div className='square-top square_2'>
-          <p className='square_2--paragraph2'>text 2</p>
+          <p className='square_2--paragraph2'>Fun !</p>
         </div>
 
         <div 
-          id='square1'
-          className={isActivate.boolOne === false ? 'square' : 'activated-square'}>
+          id={boolThree === true ? 'square1': '1'}
+          className={boolOne === false ? 'square' : 'activated-square'}>
+            <span><FaGithub /></span>
         </div>
         <div 
-          id='square2'
-          className={isActivate.boolOne === false ? 'square' : 'activated-square'}></div>
+          id={boolThree === true ? 'square2': '2'}
+          className={boolOne === false ? 'square' : 'activated-square'}>
+            <span><FaReact /></span>
+          </div>
         <div 
-          id='square3'
-          className={isActivate.boolOne === false ? 'square' : 'activated-square'}>
+          id={boolThree === true ? 'square3': '3'}
+          className={boolOne === false ? 'square' : 'activated-square'}>
+            <span><FaStackOverflow /></span>
         </div>
 
         <div 
-          id='square4'
-          className={isActivate.boolOne === true
+          id={boolThree === true ? 'square4': '4'}
+          className={boolOne === true
             ? 'activated-square' 
-            : isActivate.boolTwo === false 
-            ? 'square' 
-            : 'activated-squaretwo'}>
+            : boolTwo === false 
+              ? 'square' 
+              : 'activated-squaretwo'}>
+                <span><TbBrandNextjs /></span>
         </div>
         <div 
-          id='square5'
-          className={isActivate.boolOne === true
-            ? 'activated-square' 
-            : isActivate.boolTwo === false 
-            ? 'square' 
-            : 'activated-squaretwo'}>
+          id={boolThree === true ? 'square5': '5'}
+          className={boolOne === true
+            ? 'activated-square'
+            : boolTwo === false 
+              ? 'square' 
+              : 'activated-squaretwo'}>
           <span>😃</span>
         </div>
         <div 
-          id='square6'
-          className={isActivate.boolOne === true
+          id={boolThree === true ? 'square6': '6'}
+          className={boolOne === true
             ? 'activated-square' 
-            : isActivate.boolTwo === false 
-            ? 'square' 
-            : 'activated-squaretwo'}>
+            : boolTwo === false 
+              ? 'square' 
+              : 'activated-squaretwo'}>
+                <span><SiTypescript /></span>
         </div>
 
         <div 
-          id='square7'
-          className={isActivate.boolOne === false ? 'square' : 'activated-square'}></div>
+          id={boolThree === true ? 'square7': '7'}
+          className={boolOne === false ? 'square' : 'activated-square'}>
+            <span><FaSass /></span>
+          </div>
         <div 
-          id='square8'
-          className={isActivate.boolOne === false ? 'square' : 'activated-square'}></div>
+          id={boolThree === true ? 'square8': '8'}
+          className={boolOne === false ? 'square' : 'activated-square'}>
+            <span><SiTailwindcss /></span>
+          </div>
         <div 
-          id='square9'
-          className={isActivate.boolOne === false ? 'square' : 'activated-square'}></div>
+          id={boolThree === true ? 'square9': '9'}
+          className={boolOne === false ? 'square' : 'activated-square'}>
+            <span><SiMysql /></span>
+          </div>
 
-        <button type="button" onClick={handleClickBtn1} className='button'>Snake</button>
+        <button type="button" onClick={handleClickBtn1} className='button'>one-after-one</button>
         <button type="button" onClick={handleClickBtn2} className='button'>K-2000</button>
-        <button type="button" onClick={handleClickBtn3} className='button'>one-after-one</button>
+        <button type="button" onClick={handleClickBtn3} className='button'>
+          {boolThree === false ? "rotation" : "reset"}
+        </button>
 
       </div>
 
